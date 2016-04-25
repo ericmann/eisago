bc04100a7fca759426a9ceeb372f1bcb028bfe90
Eric Mann <eric@eamann.com>
2016-04-24 20:40:32 -0700

    Update timer scripts

--- par-timer.sh
+++ par-timer.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/env sh
-for i in {1..10}; do time ./eisago.sh import --mode=parallel --quiet; done 2>&1 | grep ^real | sed -e s/.*m// | awk '{sum += $1} END {print sum / NR}'
+#!/usr/bin/env bash
+time for i in {1..10}; do ./eisago.sh import --mode=parallel --quiet; done
